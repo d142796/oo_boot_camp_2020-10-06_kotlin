b798157014b40edc31350aa655e10a1d9ecd2af9
Fred George <fredgeorge@acm.org>
2020-10-06 13:08:02 +0200

    Fred: Chance with equality and hashCode()

---
+++ examples/src/probability/Chance.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+package probability
+
+import kotlin.math.absoluteValue
+
+// Understands the likelihood of something specific occurring
+class Chance(likelihoodAsFraction: Number) {
+    private val fraction = likelihoodAsFraction.toDouble()
+
+    override fun equals(other: Any?) = other is Chance && this.equals(other)
+
+    private fun equals(other: Chance) = this.fraction == other.fraction
+
+    override fun hashCode() = fraction.hashCode()
+}
---
+++ tests/test/unit/ChanceTest.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2020 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+package unit
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import probability.Chance
+
+// Ensures Chance operates correctly
+internal class ChanceTest {
+
+    @Test internal fun equals() {
+        assertEquals(Chance(0.75), Chance(0.75))
+        assertNotEquals(Chance(0.75), Chance(0.25))
+        assertNotEquals(Chance(0.75), Any())
+        assertNotEquals(Chance(0.75), null)
+    }
+
+    @Test fun `Chance in hash set`() {
+        assert(hashSetOf(Chance(0.75)).contains(Chance(0.75)))
+        assertEquals(1, hashSetOf(Chance(0.75), Chance(0.75)).size)
+    }
+
+    @Test fun hash() {
+        assertEquals(Chance(0.75).hashCode(), Chance(0.75).hashCode())
+    }
+}
